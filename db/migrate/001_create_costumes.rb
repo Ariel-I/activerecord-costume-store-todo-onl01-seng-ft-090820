6c6c4e1d9711dd5d52521656b0bc6a99d03937d4
Ariel Ibarra <arielibarra39@gmail.com>
2020-10-12 23:04:04 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -7,7 +7,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.string :size 
       t.text :image_url
       t.datetime :created_at 
-      t.timestamp :updated_at 
+      t.datetime :updated_at 
     end 
   end 
   
